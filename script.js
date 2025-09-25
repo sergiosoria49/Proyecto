b23d76bea0cbebe93ecc3dac694e05590f84a00c
Sergio Soria <sergio.soria@istea.com.ar>
2025-09-24 23:58:47 -0300

    Creando carousel sobre mi

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// const iconoHamburguesa = document.querySelector('hamburger')
-// const hamburguesa = document.querySelector('navbar__nav')
 
-// iconoHamburguesa.addEventListener('click', () =>{
-//     alert("hola")
-// })
-
+// metodo hamburgesa 
 const hamburger = document.querySelector('.hamburger')
 const navLinks = document.querySelector('.navbar__nav')
 const navLinksLogin = document.querySelector('.navbar__nav__login')
@@ -14,6 +9,7 @@ hamburger.addEventListener('click', () => {
   navLinksLogin.classList.toggle('active')
 });
 
+// Calcular la altura de la pantalla 
 function setVH(){
   // window.innerHeight = alto visible real en pixeles
   let vh = window.innerHeight * 0.01;
@@ -23,3 +19,29 @@ function setVH(){
 setVH();
 
 window.addEventListener('resize',setVH);
+
+// carousel
+
+const track = document.querySelector('.about-me__carousel-wrapper')
+const slides = Array.from(track.children)
+const nextButton = document.querySelector('.next')
+const prevButton = document.querySelector('.prev')
+
+let currentIndex = 0;
+
+function updateSlide() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+  }
+
+  nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateSlide();
+  });
+
+  prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateSlide();
+  });
+
+  window.addEventListener('resize', updateSlide);
